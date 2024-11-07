b3c0b50e9c2f4b21d99f5638ee735b4d9585cdcf
Desenvolvedor 5 <nddmsdn05@nddigital.com.br>
2024-11-07 15:58:32 -0300

    Criado metodos GetId, Put e Delete

---
+++ GestaoLivraria/Models/Enums/EnumGender.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace GestaoLivraria.Models.Enums
+{
+    public enum EnumGender
+    {
+        [Description("Ficção")]
+        ficcao = 0,
+        [Description("Romance")]
+        romance = 1,
+        [Description("Mistério")]
+        misterio = 2,
+        [Description("Drama")]
+        drama = 3,
+        [Description("Ação")]
+        acao = 4
+    }
+}
